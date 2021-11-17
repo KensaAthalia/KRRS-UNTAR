003de723ccf26f5190afb4e9cff6d05d750e7545
VennyCyntia <88637727+VennyCyntia@users.noreply.github.com>
2021-11-17 18:18:10 +0700

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,19 @@ router.get('/login', async (req, res) => {
     res.render('pages/login',{layout:false});
 })
 
+router.get('/lupapass',(req,res) =>{
+    res.render('pages/lupapass',{layout:false});
+})
+router.get('/lupapass2',(req,res) =>{
+    res.render('pages/lupapass2',{layout:false});
+})
+router.get('/lupapass3',(req,res) =>{
+    res.render('pages/lupapass3',{layout:false});
+})
+router.get('/lupapass4',(req,res) =>{
+    res.render('pages/lupapass4',{layout:false});
+})
+
 router.get('/logout', async (req, res) => {
     req.session.isLoggedIn = false;
     res.redirect('/');
@@ -32,6 +45,27 @@ router.post('/login', async (req,res) => {
             else {
                 res.render('pages/login', {layout:false,error: 'Wrong Email or Password!'})
             }
+        }
+    })
+})
+
+router.post('/forgot', async (req,res) => {
+    const email = req.body.email;
+
+    data = await User.find();
+
+    await data.forEach((account) => {
+        if (email == account.email) {
+            res.redirect('/lupapass3');
+                if (password.NewPassword == ConfirmPassword) {
+                    account.password == NewPassword;
+                    account.save((error,savedUser)=>{
+                        if(error) throw error
+                        res.redirect('/lupapass4');
+                    })
+                 }else {
+                    res.render('/lupapass2', {layout:false,error: 'Password is not match'})
+                    }
         }
     })
 })

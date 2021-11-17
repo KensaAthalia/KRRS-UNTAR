99ecdf299b1174c6b3c0908b216169fe689b2c48
VennyCyntia <88637727+VennyCyntia@users.noreply.github.com>
2021-11-17 20:56:08 +0700

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -42,7 +42,43 @@ router.post('/login', async (req,res) => {
                     res.redirect('/admin/adminHome');
                 }
             }
-            else {
+            else {<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pengisian KRRS Online</title>
+  <link rel="stylesheet" href="assets/Style.css">
+</head>
+
+<body>
+  <div class="bg_image">
+    <div class="header">
+      <img src="images/untarputih.png" alt="logo" style="width: 50px; margin-left: 8%; margin-top: 3px;">
+      <h1 style="margin-top: -60px; margin-left: 500px; font-size: 30pt; color:white;">Pengisian KRRS Online</h1>
+    </div>
+  </div>
+  <div class="center" style="margin-left: -28%; margin-top: 3%; height:250px; ">
+    <form method="post" action="/auth/cekEmail" style="padding-top: 50px;">
+      <label for="email" style="font-weight: bolder;">Masukkan Email</label>
+      <input type="text" id="email" name="email" placeholder="Email">
+      <a href="/auth/login" style="color:#A01627; font-size: 10pt; text-decoration: none">Kembali ke login page</a>
+      <div style="margin-top:22px; margin-left:135px; ">
+        <input type="submit" value="continue"
+          style="background-color:#A01627; color:white; font-size: 13pt; width:175px;">
+      </div>
+    </form>
+    <% if(locals.error) { %>
+      <span class="text-danger pl-2" style="font-size: 2.2vh; margin-left: 5%;">
+        <%= error %>
+      </span>
+      <% } %>
+  </div>
+</body>
+
+</html>
                 res.render('pages/login', {layout:false,error: 'Wrong Email or Password!'})
             }
         }

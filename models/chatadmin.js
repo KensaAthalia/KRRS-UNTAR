969edcd0a6580ca0ba33b45847697383f6141f73
KensaAthalia <88992468+KensaAthalia@users.noreply.github.com>
2021-11-19 13:02:53 +0700

    Add files via upload

---
+++ models/chatadmin.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const chtadminSchema = mongoose.Schema({
+    nim:{
+        type:Number
+    },
+    pertanyaan:{
+        type: String,
+        required: true
+    },
+    jawaban:{
+        type: String,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+module.exports = mongoose.model('chatadmin', chtadminSchema)
